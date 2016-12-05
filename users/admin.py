fe53c924fb90043b55b4e2050d043c8712685685
Sandesh Sanjeev <sandesh.achr@gmail.com>
2016-12-05 16:40:49 -0600

    Initial commit for waitLine.

---
+++ users/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Student, Professor, Line
+
+admin.site.register(Student)
+admin.site.register(Professor)
+admin.site.register(Line)
---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Professor(models.Model):
+    """
+    Class modelling the professors available for consultation.
+    """
+    prof_id = models.AutoField(primary_key=True)
+    # Enter the full name of the professor
+    prof_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.prof_name
+
+
+class Student(models.Model):
+    """
+    Class modelling the students waiting in line.
+    """
+    student_id = models.AutoField(primary_key=True)
+    student_name = models.CharField(max_length=50)
+    mav_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.student_name
+
+
+class Line(models.Model):
+    """
+    Class modelling the wait line.
+    """
+    position = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, to_field='student_id')
+    prof_id = models.ForeignKey(Professor, to_field='prof_id')
+
+    def __str__(self):
+        return str(self.prof_id) + " : " + str(self.student_id)
